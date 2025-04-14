8a7cae014fc0f6bd218ebcd83fe72f131a502625
Paul Picazo <ppicazo@gmail.com>
2025-04-13 19:42:33 -0700

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cachecade",
-    version="0.1.0",
+    version="0.2.0",
     author="Your Name",
     author_email="paul@picazo.com",
     description="A flexible caching decorator for Flask that supports Replit, Redis, and in-memory storage.",
